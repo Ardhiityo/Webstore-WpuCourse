add1958e80958acc731d1e94b72c6959e09c711c
belajar-vue-2 <aryaadi229@gmail.com>
2025-08-19 20:33:38 +0700

    feat: Enhance ProductCatalog with filtering, sorting, and search functionality

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -7,19 +7,64 @@ use App\Models\Tag;
 use App\Models\Product;
 use Livewire\Component;
 use App\Data\ProductData;
+use Livewire\WithPagination;
 
 class ProductCatalog extends Component
 {
+    use WithPagination;
+
+    public $keyword = '';
+    public $collections = [];
+    public $sort_by = 'latest';
+
+    public function applyFilter()
+    {
+        $this->resetPage();
+    }
+
+    public function resetFilter()
+    {
+        $this->reset();
+        $this->resetPage();
+    }
+
     public function render()
     {
-        $products = Product::paginate(9);
+        $query = Product::query();
 
-        ProductData::collect($products);
+        if ($this->keyword) {
+            $query->whereLike('name', '%' . $this->keyword . '%');
+        }
+
+        if (!empty($this->collections)) {
+            $query->whereHas('tags', function ($q) {
+                $q->whereIn('id', $this->collections);
+            });
+        }
+
+        switch ($this->sort_by) {
+            case 'latest':
+                $query->latest();
+                break;
+            case 'oldest':
+                $query->oldest();
+                break;
+            case 'price_asc':
+                $query->orderBy('price', 'asc');
+                break;
+            case 'price_desc':
+                $query->orderBy('price', 'desc');
+                break;
+            default:
+                $query->latest();
+        }
+
+        $products = ProductData::collect($query->paginate(9));
 
         $tags = Tag::query()->withType('collection')->withCount('products')->get();
 
-        $collections = ProductCollectionData::collect($tags);
+        ProductCollectionData::collect($tags);
 
-        return view('livewire.product-catalog', compact('products', 'collections'));
+        return view('livewire.product-catalog', compact('products', 'tags'));
     }
 }
