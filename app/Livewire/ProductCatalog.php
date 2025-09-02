c23eab96e1ee93f554fb7ef88c6b6171be6a6b61
belajar-vue-2 <aryaadi229@gmail.com>
2025-09-02 19:54:58 +0700

    feat : Passing Data To View (Basic)

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class ProductCatalog extends Component
 {
     public function render()
     {
-        return view('livewire.product-catalog');
+        $products = Product::paginate(6);
+
+        return view('livewire.product-catalog', compact('products'));
     }
 }
