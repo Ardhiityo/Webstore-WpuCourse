fa8c9ea7a6aa6193cbb6342d2649e3428024fa59
belajar-vue-2 <aryaadi229@gmail.com>
2025-09-05 13:21:30 +0700

    feat : Front Store: Passing DTO Product Detail

--- app/Data/ProductData.php
+++ app/Data/ProductData.php
@@ -8,6 +8,7 @@ use App\Models\Product;
 use Spatie\LaravelData\Data;
 use Illuminate\Support\Number;
 use Spatie\LaravelData\Attributes\Computed;
+use Spatie\LaravelData\Optional;
 
 class ProductData extends Data
 {
@@ -23,12 +24,13 @@ class ProductData extends Data
         public string $slug,
         public float $price,
         public int $weight,
-        public string $cover_url
+        public string $cover_url,
+        public array|Optional $gallery = new Optional()
     ) {
         $this->price_formatted = Number::currency($price);
     }
 
-    public static function fromModel(Product $product): self
+    public static function fromModel(Product $product, bool $with_gallery = false): self
     {
         return new self(
             $product->name,
@@ -39,7 +41,10 @@ class ProductData extends Data
             $product->slug,
             (float)$product->price,
             $product->weight,
-            $product->getFirstMediaUrl('cover')
+            $product->getFirstMediaUrl('cover'),
+            $with_gallery ? $product->getMedia('gallery')->map(
+                fn($record) => $record->getUrl()
+            )->toArray() : new Optional()
         );
     }
 }
